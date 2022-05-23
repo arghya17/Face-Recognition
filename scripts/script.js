81587135de573ed3476bb72c7f5c94ad78f681a0
arghya17 <arghyadeepsaha17@gmail.com>
2022-05-23 22:48:51 +0530

    Face detection results added via canvas

--- scripts/script.js
+++ scripts/script.js
@@ -22,11 +22,21 @@ document.addEventListener("DOMContentLoaded", () => {
   video.addEventListener("play", () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
+    const displaysize = {
+      width: video.width,
+      height: video.height,
+    };
+    faceapi.matchDimensions(canvas, displaysize);
     setInterval(async () => {
       const detections = await faceapi
         .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
         .withFaceLandmarks()
         .withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaysize);
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
     }, 100);
   });
 });
